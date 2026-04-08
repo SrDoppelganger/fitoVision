58b6b9eb67e4b1e3d4736fd22d629789b2cad3b3
SrDoppelganger <arthur.albuquerque2004@gmail.com>
2026-04-08 19:20:54 -0300

    dando os primeiros passos fr fr

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("hello,world")
+import cv2 as cv
+
+#analisa a imagem
+imgPath = 'IMGs/pexels-cartist-3605370.jpg';
+image = cv.imread(imgPath);
+
+#configuração da janela
+cv.namedWindow("girassol",cv.WINDOW_NORMAL)
+cv.resizeWindow("girassol", 800, 800)
+
+
+#mostra a imagem
+cv.imshow('girassol',image)
+
+cv.waitKey(0)
+cv.destroyAllWindows()
